e39ce8cfec85eb4623353f3a4f15c84e0255395d
Uttam <kumaruttam519@gmail.com>
2025-03-22 00:17:48 +0530

    changes in files

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ import { dark } from "@clerk/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sensai - The Master",
+  title: "hirePrepAI - Your Friendly Guide",
   description: "",
 };
 
--- components/hero.jsx
+++ components/hero.jsx
@@ -30,14 +30,13 @@ const HeroSection = () => {
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
-          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
-            Your AI Career Coach for
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl animate-gradient">
+          AI-Powered Tools for Your 
             <br />
-            Professional Success
+            Career Growth
           </h1>
           <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
-            Advance your career with personalized guidance, interview prep, and
-            AI-powered tools for job success.
+          Achieve job success with AI-driven support, interview prep, and career insights.
           </p>
         </div>
         <div className="flex justify-center space-x-4">
